e8b8594774fad9e5bc552a647641d525da427431
panda-sas <panda.sas20@gmail.com>
2021-11-04 15:16:15 -0500

    Basic file opening, reading and writing

---
+++ Files - Directories and Paths/main.py
@@ -0,0 +1,19 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# Reading a file
+with open("my_file.txt") as f:
+    contents = f.read()
+    print(contents)
+
+# Writing a file
+with open("my_file.txt", mode="a") as file:
+    file.write("\nThis is another new line of text. ")
+
+# Creating a file
+with open("another_file.text", mode="w") as file_2:
+    file_2.write("This is a second file. ")
+
+
--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -19,7 +19,6 @@ class Snake:
         for position in START_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
@@ -27,6 +26,12 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
