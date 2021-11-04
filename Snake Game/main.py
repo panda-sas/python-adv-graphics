bfe978e88973e956994c3442af1d7f6108b71c58
panda-sas <panda.sas20@gmail.com>
2021-11-04 14:15:01 -0500

    Snake-Game part 2 - detecting collision with tail & wall and keeping a scoreboard using Inheritance and list slicing

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -18,7 +18,7 @@ screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
-
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
