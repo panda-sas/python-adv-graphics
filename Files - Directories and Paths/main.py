b80ba690d43d4f2a96b11dffbd6d8c32f1c6c1ca
panda-sas <panda.sas20@gmail.com>
2021-11-04 16:54:30 -0500

    Reading a file using Readlines(),replacing a placeholder with the names returned from the list using Replace() and stripping unnecessary newlines using Strip() to send out a invite letter by writing into a new file.

--- Files - Directories and Paths/main.py
+++ Files - Directories and Paths/main.py
@@ -4,16 +4,16 @@
 # file.close()
 
 # Reading a file
-with open("my_file.txt") as f:
+with open("/Users/Saswati Panda/OneDrive/Desktop/my_file.txt") as f:
     contents = f.read()
     print(contents)
 
 # Writing a file
-with open("my_file.txt", mode="a") as file:
-    file.write("\nThis is another new line of text. ")
+with open("/Users/Saswati Panda/OneDrive/Desktop/my_file.txt", mode="a") as file:
+    file.write("\nThis is another new line of text - v2. ")
 
 # Creating a file
-with open("another_file.text", mode="w") as file_2:
-    file_2.write("This is a second file. ")
+# with open("another_file.text", mode="w") as file_2:
+#     file_2.write("This is a second file. ")
 
 
