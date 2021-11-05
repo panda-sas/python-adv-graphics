fc577b60d8f05c8c6efbbdf5a9ea949cdf5178fa
panda-sas <panda.sas20@gmail.com>
2021-11-05 09:40:19 -0500

    Guess the states in US, Setup the game, Keep score, keep track of missing states and saving that data to csv. Applying List Comprehension to the game

--- Files - Directories and Paths/US States Game/main.py
+++ Files - Directories and Paths/US States Game/main.py
@@ -16,10 +16,8 @@ while len(guessed_states) < 50:
     data = pandas.read_csv("50_states.csv")
     all_states = data.state.to_list()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
