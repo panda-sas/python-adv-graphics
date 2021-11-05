cda38b67af17e58fc40f5e058c8ab93e4258831c
panda-sas <panda.sas20@gmail.com>
2021-11-05 12:30:15 -0500

    Iterating over a pandas dataframe

---
+++ Files - Directories and Paths/NATO Alphabet series/main.py
@@ -0,0 +1,13 @@
+import pandas
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+student_df = pandas.DataFrame(student_dict)
+# print(student_df)
+
+# Loop through rows of a data frame
+for (index, row) in student_df.iterrows():
+    if row.student == "Angela":
+        print(row.score)
