7c01620826b4c3d7dc02a6a8b871e2666dbd6b4a
panda-sas <panda.sas20@gmail.com>
2021-11-08 09:23:38 -0600

    NATO alphabet _ phonetic code words from a word that the user inputs, exception handling

--- Files - Directories and Paths/NATO Alphabet series/NATO-alphabet-start/main.py
+++ Files - Directories and Paths/NATO Alphabet series/NATO-alphabet-start/main.py
@@ -1,10 +1,21 @@
 import pandas
+
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
